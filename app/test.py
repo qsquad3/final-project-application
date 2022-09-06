aaf67205d5a53cd2a618c3387b50462561aa6943
Gabriel Mariusso Caldeira <gabriel_mariusso@hotmail.com>
2022-09-05 23:43:19 -0300

    add pytest and jenkinsfile

---
+++ app/test.py
@@ -0,0 +1,37 @@
+import unittest
+
+class TestApp():
+
+    def test_app():
+        pass
+
+    # def setUp(self):
+    #     app.app.testing = True
+    #     self.app = app.app.test_client()
+
+    # def setUp(self):
+    #     app.app.testing = True
+    #     self.app = app.app.test_client()
+
+    # def test_hello(self):
+    #     rv = self.app.get('/')
+    #     self.assertEqual(rv.status, '200 OK')
+    #     self.assertEqual(rv.data, b'Hello World!\n')
+
+    # def test_healthcheck(self):
+    #     rv = self.app.get('/healthcheck/')
+    #     self.assertEqual(rv.status, '200 OK')
+    #     self.assertEqual(rv.data, b'Hello World!\n')
+
+    # def test_limpar(self):
+    #     rv = self.app.get('/limpar/')
+    #     self.assertEqual(rv.status, '200 OK')
+    #     self.assertEqual(rv.data, b'Hello World!\n')
+
+    # def test_popular(self):
+    #     rv = self.app.get('/popular/')
+    #     self.assertEqual(rv.status, '200 OK')
+    #     self.assertEqual(rv.data, b'Hello World!\n')
+
+if __name__ == '__main__':
+    unittest.main()
